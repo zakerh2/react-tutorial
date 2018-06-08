fe7ed832407ea4f2017968f2579f095b4e61c753
Zaker Husien <zakerh2@gmail.com>
2018-06-08 16:44:13 +0200

    React Clock

--- src/index.js
+++ src/index.js
@@ -1,9 +1,13 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
 
-ReactDOM.render(
+//create clock in React
 
-		<h1>Welcome to my React JS</h1>,
+setInterval(function(){
+	ReactDOM.render(
+
+		 <h1>The local time is: {new Date().toLocaleTimeString()}</h1>,
 
 		document.getElementById('root')
 	);
+},1000);
