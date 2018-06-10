eb9c509ef59aca6618a78dcb3b00887fcc3f83d4
Zaker Husien <zakerh2@gmail.com>
2018-06-10 15:36:26 +0200

    clock Advanced

--- src/index.js
+++ src/index.js
@@ -3,19 +3,54 @@ import ReactDOM from 'react-dom'
 
 //create clock in React
 
-function Clock()
-{
-	return <h1>The local time is: {new Date().toLocaleTimeString()}</h1>
+// function Clock()
+// {
+// 	return <h1>The local time is: {new Date().toLocaleTimeString()}</h1>
 
+// }
+
+class Clock extends React.Component
+{
+	constructor(props)
+	{
+		super(props);
+		this.state = {
+			date: new Date()
+		}
+
+	}
+
+	//this function runs when the the page is loadid
+
+	componentDidMount()
+	{
+		this.timer = setInterval(() => this.start(),1000)
+
+	}
+
+    //this function run when the the page is to close
+	componentWillUnmount()
+	{
+		clearInterval(this.timer);
+	}
+
+	start()
+	{
+		this.setState({
+			date: new Date()
+		});
+	}
+
+	render(){
+		return <h1>The local time is: {this.state.date.toLocaleTimeString()}</h1>
+	}
 }
 
-function time()
-{
+/// render the Dom
 	ReactDOM.render(
      <Clock />,
 
 		document.getElementById('root')
 	);
-}
 
-setInterval(function(){time()},1000);
+
